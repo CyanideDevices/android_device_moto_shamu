2d615826d20b545661ef145171eff31c3bca416a
msifland <msifland@gmail.com>
2015-12-10 13:46:16 -0600

    Add prox sensor option

--- vrtoxin.mk
+++ vrtoxin.mk
@@ -12,6 +12,7 @@ $(call inherit-product, vendor/vrtoxin/config/nfc_enhanced.mk)
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-cm
+DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := vrtoxin_shamu
